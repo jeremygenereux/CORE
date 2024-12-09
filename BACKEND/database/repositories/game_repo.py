843922ee23465b5c5825aee97ecfd2fc36e0db8e
Jérémy Généreux <jeremy@asteryx.digital>
2024-12-09 01:48:37 -0500

    Delete BACKEND directory

--- BACKEND/database/repositories/game_repo.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import desc
-from sqlalchemy.orm import Session
-from database.models.game import Game
-
-def get_team_current_game(session: Session, team_id) -> Game | None:
-    # Return the game underway (most recent if multiple)
-    return session.query(Game).filter(Game.is_underway == True, Game.team_id == team_id).order_by(desc(Game.start_ts)).first()
